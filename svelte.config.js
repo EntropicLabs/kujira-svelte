ce96d776931547b170d723b8a1d9926a9592f020
Amit Prasad <17amitprasad@gmail.com>
2023-09-26 19:27:17 -0500

    adapter-static, polyfills

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import { preprocessMeltUI } from "@melt-ui/pp";
 import sequence from "svelte-sequential-preprocessor";
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      fallback: 'index.html'
+    })
   }
 };
 export default config;
