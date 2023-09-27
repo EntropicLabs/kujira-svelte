28720a3c84adb727d6248907ff0061fde491ea7d
Amit Prasad <17amitprasad@gmail.com>
2023-09-26 19:14:05 -0500

    removing boilerplate from sveltekit template

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,4 @@
 import 'unplugin-icons/types/svelte';
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
